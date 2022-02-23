3772dd519164e3014d446e987e76b64ac531b599
meedbek <c_mohammed@hotmail.fr>
2022-02-23 02:00:24 +0100

    model ready to be deployed

---
+++ OurFunctions.py
@@ -0,0 +1,375 @@
+import pandas as pd
+import numpy as np
+import nltk as nk
+import re 
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from torch.utils.data import Dataset
+# Thanks to https://www.kaggle.com/rftexas/text-only-kfold-bert
+abbreviations = {
+    "$" : " dollar ",
+    "€" : " euro ",
+    "4ao" : "for adults only",
+    "a.m" : "before midday",
+    "a3" : "anytime anywhere anyplace",
+    "aamof" : "as a matter of fact",
+    "acct" : "account",
+    "adih" : "another day in hell",
+    "afaic" : "as far as i am concerned",
+    "afaict" : "as far as i can tell",
+    "afaik" : "as far as i know",
+    "afair" : "as far as i remember",
+    "afk" : "away from keyboard",
+    "app" : "application",
+    "approx" : "approximately",
+    "apps" : "applications",
+    "asap" : "as soon as possible",
+    "asl" : "age, sex, location",
+    "atk" : "at the keyboard","ave." : "avenue",
+    "aymm" : "are you my mother",
+    "ayor" : "at your own risk", 
+    "b&b" : "bed and breakfast",
+    "b+b" : "bed and breakfast",
+    "b.c" : "before christ",
+    "b2b" : "business to business",
+    "b2c" : "business to customer",
+    "b4" : "before",
+    "b4n" : "bye for now",
+    "b@u" : "back at you",
+    "bae" : "before anyone else",
+    "bak" : "back at keyboard",
+    "bbbg" : "bye bye be good",
+    "bbc" : "british broadcasting corporation",
+    "bbias" : "be back in a second",
+    "bbl" : "be back later",
+    "bbs" : "be back soon",
+    "be4" : "before",
+    "bfn" : "bye for now",
+    "blvd" : "boulevard", "bout" : "about",
+    "brb" : "be right back",
+    "bros" : "brothers",
+    "brt" : "be right there",
+    "bsaaw" : "big smile and a wink",
+    "btw" : "by the way",
+    "bwl" : "bursting with laughter",
+    "c/o" : "care of",
+    "cet" : "central european time",
+    "cf" : "compare",
+    "cia" : "central intelligence agency",
+    "csl" : "can not stop laughing",
+    "cu" : "see you",
+    "cul8r" : "see you later",
+    "cv" : "curriculum vitae",
+    "cwot" : "complete waste of time",
+    "cya" : "see you",
+    "cyt" : "see you tomorrow",
+    "dae" : "does anyone else",
+    "dbmib" : "do not bother me i am busy",
+    "diy" : "do it yourself",
+    "dm" : "direct message",
+    "dwh" : "during work hours",
+    "e123" : "easy as one two three",
+    "eet" : "eastern european time",
+    "eg" : "example",
+    "embm" : "early morning business meeting",
+    "encl" : "enclosed",
+    "encl." : "enclosed",
+    "etc" : "and so on",
+    "faq" : "frequently asked questions",
+    "fawc" : "for anyone who cares",
+    "fb" : "facebook",
+    "fc" : "fingers crossed",
+    "fig" : "figure",
+    "fimh" : "forever in my heart", 
+    "ft." : "feet",
+    "ft" : "featuring",
+    "ftl" : "for the loss",
+    "ftw" : "for the win",
+    "fwiw" : "for what it is worth",
+    "fyi" : "for your information",
+    "g9" : "genius",
+    "gahoy" : "get a hold of yourself",
+    "gal" : "get a life",
+    "gcse" : "general certificate of secondary education",
+    "gfn" : "gone for now",
+    "gg" : "good game",
+    "gl" : "good luck",
+    "glhf" : "good luck have fun",
+    "gmt" : "greenwich mean time",
+    "gmta" : "great minds think alike",
+    "gn" : "good night",
+    "g.o.a.t" : "greatest of all time",
+    "goat" : "greatest of all time",
+    "goi" : "get over it",
+    "gps" : "global positioning system",
+    "gr8" : "great",
+    "gratz" : "congratulations",
+    "gyal" : "girl",
+    "h&c" : "hot and cold",
+    "hp" : "horsepower",
+    "hr" : "hour",
+    "hrh" : "his royal highness",
+    "ht" : "height",
+    "ibrb" : "i will be right back",
+    "ic" : "i see",
+    "icq" : "i seek you",
+    "icymi" : "in case you missed it",
+    "idc" : "i do not care",
+    "idgadf" : "i do not give a damn fuck",
+    "idgaf" : "i do not give a fuck",
+    "idk" : "i do not know",
+    "ie" : "that is",
+    "i.e" : "that is",
+    "ifyp" : "i feel your pain",
+    "IG" : "instagram",
+    "iirc" : "if i remember correctly",
+    "ilu" : "i love you",
+    "ily" : "i love you",
+    "imho" : "in my humble opinion",
+    "imo" : "in my opinion",
+    "imu" : "i miss you",
+    "iow" : "in other words",
+    "irl" : "in real life",
+    "j4f" : "just for fun",
+    "jic" : "just in case",
+    "jk" : "just kidding",
+    "jsyk" : "just so you know",
+    "l8r" : "later",
+    "lb" : "pound",
+    "lbs" : "pounds",
+    "ldr" : "long distance relationship",
+    "lmao" : "laugh my ass off",
+    "lmfao" : "laugh my fucking ass off",
+    "lol" : "laughing out loud",
+    "ltd" : "limited",
+    "ltns" : "long time no see",
+    "m8" : "mate",
+    "mf" : "motherfucker",
+    "mfs" : "motherfuckers",
+    "mfw" : "my face when",
+    "mofo" : "motherfucker",
+    "mph" : "miles per hour",
+    "mr" : "mister",
+    "mrw" : "my reaction when",
+    "ms" : "miss",
+    "mte" : "my thoughts exactly",
+    "nagi" : "not a good idea",
+    "nbc" : "national broadcasting company",
+    "nbd" : "not big deal",
+    "nfs" : "not for sale",
+    "ngl" : "not going to lie",
+    "nhs" : "national health service",
+    "nrn" : "no reply necessary",
+    "nsfl" : "not safe for life",
+    "nsfw" : "not safe for work",
+    "nth" : "nice to have",
+    "nvr" : "never",
+    "nyc" : "new york city",
+    "oc" : "original content",
+    "og" : "original",
+    "ohp" : "overhead projector",
+    "oic" : "oh i see",
+    "omdb" : "over my dead body",
+    "omg" : "oh my god",
+    "omw" : "on my way",
+    "p.a" : "per annum",
+    "p.m" : "after midday",
+    "pm" : "prime minister",
+    "poc" : "people of color",
+    "pov" : "point of view",
+    "pp" : "pages",
+    "ppl" : "people",
+    "prw" : "parents are watching",
+    "ps" : "postscript",
+    "pt" : "point",
+    "ptb" : "please text back",
+    "pto" : "please turn over",
+    "qpsa" : "what happens", #"que pasa",
+    "ratchet" : "rude",
+    "rbtl" : "read between the lines",
+    "rlrt" : "real life retweet", 
+    "rofl" : "rolling on the floor laughing",
+    "roflol" : "rolling on the floor laughing out loud",
+    "rotflmao" : "rolling on the floor laughing my ass off",
+    "rt" : "retweet",
+    "ruok" : "are you ok",
+    "sfw" : "safe for work",
+    "sk8" : "skate",
+    "smh" : "shake my head",
+    "sq" : "square",
+    "srsly" : "seriously", 
+    "ssdd" : "same stuff different day",
+    "tbh" : "to be honest",
+    "tbs" : "tablespooful",
+    "tbsp" : "tablespooful",
+    "tfw" : "that feeling when",
+    "thks" : "thank you",
+    "tho" : "though",
+    "thx" : "thank you",
+    "tia" : "thanks in advance",
+    "til" : "today i learned",
+    "tl;dr" : "too long i did not read",
+    "tldr" : "too long i did not read",
+    "tmb" : "tweet me back",
+    "tntl" : "trying not to laugh",
+    "ttyl" : "talk to you later",
+    "u" : "you",
+    "u2" : "you too",
+    "u4e" : "yours for ever",
+    "utc" : "coordinated universal time",
+    "w/" : "with",
+    "w/o" : "without",
+    "w8" : "wait",
+    "wassup" : "what is up",
+    "wb" : "welcome back",
+    "wtf" : "what the fuck",
+    "wtg" : "way to go",
+    "wtpa" : "where the party at",
+    "wuf" : "where are you from",
+    "wuzup" : "what is up",
+    "wywh" : "wish you were here",
+    "yd" : "yard",
+    "ygtr" : "you got that right",
+    "ynk" : "you never know",
+    "zzz" : "sleeping bored and tired"
+}
+
+def lowerTweet(tweet):
+    return tweet.lower()
+
+def removeURLs(tweet):
+        return re.sub(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', " ", tweet)
+
+def toAscii(tweet):
+  return  (tweet.encode('ascii',errors='ignore')).decode('ascii') 
+
+
+def removeSpecialChar(text):
+    text = list(text)
+    
+    #remove special character & ponctuations & numbers
+    listOfSpecialChars =  [*range(33,48),*range(58,65),*range(91,97),*range(123,127)]
+    listOfSpecialChars.remove(39) #dont include ' in the list
+    
+    text = [char for char in text if (ord(char) not in listOfSpecialChars and not char.isdigit())] 
+    text = ''.join(text)
+    
+    return text
+
+def removeApos(text):
+    text = text.replace("'",'')
+    return text
+
+def convert_abbrev(word):
+    return abbreviations[word.lower()] if word.lower() in abbreviations.keys() else word
+
+def convert_abbrev_in_text(text):
+    tokens = text.split(' ')
+    tokens = [convert_abbrev(word) for word in tokens]
+    text = ' '.join(tokens)
+    return text
+
+
+
+def removeLetter(text):
+    text = text.split()
+    text = [word for word in text if  len(word) > 1]
+    text = ' '.join(text)
+    return text
+
+
+def removeStopWords(text):
+    sw = stopwords.words("english")
+    text = text.split()
+    text = [word for word in text if word not in sw]
+    return ' '.join(text)
+
+
+
+
+
+def stem(text):
+    ps = PorterStemmer()
+    text = text.split()
+    text= [ps.stem(word) for word in text]
+    return ' '.join(text)
+
+
+def preprocess(text):
+    text = lowerTweet(text)
+    text = removeURLs(text)
+    text = toAscii(text)
+    text = removeSpecialChar(text)
+    text = removeStopWords(text)
+    text = removeApos(text)
+    text = removeStopWords(text)
+    text = removeLetter(text)
+    text = stem(text)
+    text = convert_abbrev_in_text(text)
+    return text
+
+def tokenize(text):
+    return nk.word_tokenize(text)
+
+
+def bagOfWords(df):
+    tokens = []
+    bag = {}
+    for tweet in df.tweet:
+        tokens += tokenize(preprocess(tweet))
+    for token in tokens:
+        if token not in bag.keys():
+            bag[token] = 1
+        else:
+            bag[token] += 1
+    return bag
+
+def TfIdfVec(df):
+    corpus = []
+    for  tweet in df.tweet :
+      text = preprocess(tweet)
+      corpus.append(text)
+
+    tfidf = TfidfVectorizer().fit(corpus)
+
+    tfidf_tokens = tfidf.get_feature_names_out()
+    article_vect = tfidf.transform(corpus)
+
+    df_tfidf_vect = pd.DataFrame(data=article_vect.toarray(),columns=tfidf_tokens)
+
+    return df_tfidf_vect
+
+def CountVec(df):
+    corpus = []
+    for  tweet in df.tweet :
+      text = preprocess(tweet)
+      corpus.append(text)
+
+    count = CountVectorizer().fit(corpus)
+
+    count_tokens = count.get_feature_names_out()
+    article_vect = count.transform(corpus)
+
+    df_count_vect = pd.DataFrame(data=article_vect.toarray(),columns=count_tokens)
+
+    return df_count_vect
+
+
+def redDim(dfVec,seuil1,seuil2):
+    subDf = dfVec[dfVec != 0]
+    map = subDf.count(axis = 0) <= seuil1 
+    map2 = subDf.count(axis = 0) >= (dfVec.shape[0] - seuil2) 
+    resultDf = dfVec.drop(dfVec.columns[map+map2],axis=1)
+    columns_dropped=dfVec.columns[map+map2]
+    
+    return resultDf,columns_dropped
+
+class Covid_Dataset(Dataset):
+    def __init__(self, data, targets):
+        super(Covid_Dataset, self)
+        self.data = data
+        self.targets = targets
+    def __getitem__(self, idx):
+        return self.data[idx], self.targets[idx]
+    def __len__(self):
+        return len(self.targets)
